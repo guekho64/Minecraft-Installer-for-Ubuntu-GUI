9aaf0d223934a22ff53178c126575e8f574685b7
guekho64 <guekho64@users.noreply.github.com>
2016-07-29 16:30:39 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,67 +1,152 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-## First Version Ever ##
-
 import os
 import subprocess
+import sys
+from subprocess import call
 import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from urllib import urlretrieve
 
-XML = """HERE GOES THE .GLADE FILE"""
+#Acciones Preliminares
 
-def button_1clicked(self, button):
-	from subprocess import call
+makedir = """mkdir ~/.local/share/icons""" + " >/dev/null 2>&1" 
+bash_script = """ bash -c 'Fuente="${BASH_SOURCE[0]}"
+            while [ -h "$Fuente" ]; do
+                Directorio="$( cd -P "$( dirname "$Fuente" )" && pwd )"
+                declare Fuente="$(readlink "$Fuente")"
+                [[ $Fuente != /* ]] && Fuente="$Directorio/$Fuente"
+            done
+        Directorio="$( cd -P "$( dirname "$Fuente" )" && pwd )"
+        
+        Raiz="$Directorio"
+		
+		Registro="${Raiz}/Registro.txt"
+
+		echo '' > "$Registro"
+
+		cat "/usr/bin/aria2c" > /dev/null 2>&1
+		
+		TempErrorVar="$?"
+	
+		if [ "$TempErrorVar" = "0" ]; then
+            
+             get="aria2c"
+			 GetMode="-o"
+			 NoCheckCert="--check-certificate=false"
+			 
+         else
+            
+             get="wget"
+             GetMode="-O"
+			 NoCheckCert="--no-check-certificate"
+            
+         fi;
+	
+		cat "/usr/bin/apt-fast" > /dev/null 2>&1
+		
+		TempErrorVar="$?"
+	
+		if [ "$TempErrorVar" = "0" ]; then
+            
+			 apt="apt-fast"
+			 
+		else
+		
+			 apt="apt-get"
+            
+         fi;
+		 
+		 dpkg="dpkg"
+		 
+		 Gettie () {
+	
+		if [ "$get" = "wget" ] ; then
+		
+			 get="wget"
+             GetMode="-O"
+			 NoCheckCert="--no-check-certificate"
+
+			Get () {
+	
+			( "$get" --append-output="$Registro" "$4" "$1" "${GetMode}" "$2/$3" ) > /dev/null 2>&1
+	
+			}
+	
+		elif [ "$get" = "aria2c" ] ; then
+		
+			 get="aria2c"
+			 GetMode="-o"
+			 NoCheckCert="--check-certificate=false"
+
+			Get () {
+	
+			( ( "$get" "$1" "${GetMode}" "$3" -d "$2" $4 ) >> "$Registro" )
+	
+			}
+	
+		else
+
+			Error
+	
+		fi
+		
+		}
+		
+		Gettie
+		
+		cat "$HOME/.local/share/icons/Minecraft.png"  >/dev/null 2>&1
+		
+		if [ "$?" != "0" ]; then
 			
-	Damn = 1
-			
-	print (Damn)
-			
-	output = subprocess.check_output("echo $(cat 'Some Example Dir')", shell=True)
-			
-	#call(["bash", "-c", '  foo=Hi ; export foo '])
-			
-	#hy = os.environ['foo']
-			
-	print (output)
-				
-def button_2clicked(self, button):
-	from subprocess import call
-			
-	Damn2 = 2
-			
-	print (Damn2)
-			
-	#call(["bash", "-c", '  O=8 ; echo "$H" '])
-				
+			Get "http://www.rw-designer.com/icon-image/5547-64x64x32.png" "$HOME/.local/share/icons" "Minecraft.png" "$NoCheckCert"		
+	
+		fi
+		
+		 '
+		 
+		 """
+
+os.system(makedir)
+os.system(bash_script)
+
+XML = """PUT .GLADE FILE CONTENT HERE"""
+
 def Activate_Switchy(switch, active):
-	from subprocess import call
-			
-	Damn2 = 2
-			
-	print (Damn2)
-			
-	#call(["bash", "-c", '  O=8 ; echo "$H" '])
-			
-			
-			
+	if myswitch.get_active():
+		mytext.set_sensitive(True)
+		state = "Encendido"
+	else:
+		mytext.set_sensitive(False)
+		mytext.set_text("")
+		state = "Apagado"
+	print("El Switch fue", state)
+	
 builder = Gtk.Builder()
-builder.add_from_string(XML)
+#builder.add_from_string(XML)
+#builder.add_from_file("""PUT GLADE FILE LOCATION HERE""")
 
 window = builder.get_object("window1")
 
-switch = builder.get_object('Switch_Activable')
+myswitch = builder.get_object('Switch_Activable')
+mytext = builder.get_object('Entrada_Desactivable')
+mytext.set_text("")
+mytext.set_sensitive(False)
+
+os_name = subprocess.check_output("echo $(lsb_release -is) $(lsb_release -rs) $(lsb_release -cs)", shell=True).rstrip("""\n""")
+
+os_label = builder.get_object('OS_Label')
+os_label.set_text(os_name)
 
 ournewbutton = builder.get_object("button1")
 ournewbutton2 = builder.get_object("button2")
 
 builder.get_object('Switch_Activable')
 
-switch.set_active(False)
-switch.connect('notify::active', Activate_Switchy)
-
-ournewbutton.set_label("Hello, World!")
-ournewbutton2.set_label("Nope!")
+myswitch.set_active(False)
+myswitch.connect('notify::active', Activate_Switchy)
 
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
